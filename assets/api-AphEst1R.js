9842c7339bda2bc9e5037ae70351b5e74daadb51
Yertugan <muslim.ipro@gmail.com>
2024-08-09 13:44:58 +0500

    avatars fixed

---
+++ assets/api-AphEst1R.js
@@ -0,0 +1,1 @@
+import{E as h,Q as a,G as n}from"./index-Cl3fCl8d.js";function f(){const t=h();function s(o,e){const r=a();return r?fetch(o,{...e,headers:{...e==null?void 0:e.headers,Authorization:`Bearer ${r.access_token}`}}).then(u=>(u.status===401&&(n(),t.push({path:"/login"})),u)):(t.push({path:"/login"}),new Promise(()=>{}))}return{fetchAPI:s}}export{f as u};
