1858973ad765afa5c3eaca7af6228cc43a319dfd
Yertugan <muslim.ipro@gmail.com>
2024-10-02 00:52:48 +0500

    step tooltip added

---
+++ assets/ProjectService-BSDwhsrk.js
@@ -0,0 +1,1 @@
+import{a as t}from"./api-client-DOMkBDN8.js";const a={async getProjects(e){return(await t.get(`/projects/?language=${e}`)).data},async getProjectByID(e){return(await t.get("/projects/"+e)).data}};export{a as P};
