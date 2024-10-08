b47232cdd108cb150efb5a3a3b11c30a53984180
Yertugan <muslim.ipro@gmail.com>
2024-10-08 14:14:36 +0500

    unused files removed, locale watch bug fixed

---
+++ assets/ProfileService-Bpq8AuPM.js
@@ -0,0 +1,1 @@
+import{a as s}from"./api-client-B6Mm-yR_.js";const n={async getUserProfile(){return(await s.get("/user/profile")).data},async changeUserPassword(e,r){return(await s.post("/user/change_password",{old:e,new:r})).data},async getMe(){return(await s.get("/user/me")).data}};export{n as P};
