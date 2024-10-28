520b872036b4c9cd9e4831ef062dba7ca954a162
Yertugan <muslim.ipro@gmail.com>
2024-10-28 23:53:45 +0500

    rtzero returned, unarchive class added, reset student password added

--- assets/ProfileService-C2Owut9B.js
+++
@@ -1,1 +0,0 @@
-import{a as s}from"./api-client-COTgTXz3.js";const n={async getUserProfile(){return(await s.get("/user/profile")).data},async changeUserPassword(e,r){return(await s.post("/user/change_password",{old:e,new:r})).data},async getMe(){return(await s.get("/user/me")).data}};export{n as P};
