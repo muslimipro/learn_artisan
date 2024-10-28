b4a8216c9280802681b208163c71555f2f2434e9
Yertugan <muslim.ipro@gmail.com>
2024-10-28 20:38:56 +0500

    student sign up added

--- assets/artisan_logo-BSio2HZi.js
+++
@@ -1,1 +0,0 @@
-const s="/assets/artisan_logo-lZ2lJMKH.png";export{s as A};
---
+++ assets/utils-CMD4rBgk.js
@@ -0,0 +1,1 @@
+const o="/assets/artisan_logo-lZ2lJMKH.png";function s(n){const a=new URLSearchParams;for(const r in n)n.hasOwnProperty(r)&&a.append(r,n[r]);return a.toString()}export{o as A,s as o};
