f896499e38f2d9720b94a102352ce7bd753531e7
Yertugan <muslim.ipro@gmail.com>
2024-11-05 13:53:35 +0500

    course view changed

---
+++ assets/dashboard-store-CCvQ6TgH.js
@@ -0,0 +1,1 @@
+import{L as o,r as a}from"./index-Cjo14g7w.js";const n=o("useDashboardStore",()=>{const s=a([]);function e(r){s.value=r}return{classes:s,setClasses:e}});export{n as u};
---
+++ assets/logo-trY1JmHw.js
@@ -0,0 +1,1 @@
+const s="/assets/logo-Ds3l-s0O.svg";export{s as _};
